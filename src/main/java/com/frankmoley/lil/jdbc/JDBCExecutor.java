8ec7cc10c353b5f877b72113993b3f0a609379d5
Alex Frost <zufrost.a@gmail.com>
2020-05-28 17:50:44 +0300

    create DatabaseConnectionManager edit JDBCExecitor

--- src/main/java/com/frankmoley/lil/jdbc/JDBCExecutor.java
+++ src/main/java/com/frankmoley/lil/jdbc/JDBCExecutor.java
@@ -1,8 +1,24 @@
 package com.frankmoley.lil.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBCExecutor {
 
     public static void main(String... args) {
-        System.out.println("Hello Learning JDBC");
+        DatabaseConnectionManager databaseConnectionManager = new DatabaseConnectionManager("localhost",
+                "hplussport", "postgres", "9021090210");
+        try {
+            Connection connection = databaseConnectionManager.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
